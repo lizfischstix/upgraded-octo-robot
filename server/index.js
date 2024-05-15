27588ab4a8995c1becdc2964be4ed57c87c88d5b
Liz FIschenich <Liz.Devlin85@gmail.com>
2024-05-15 18:56:34 -0500

    add basic routes

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 const db = require("./models");
+
+//Routers
+const postRouter = require("./routes/Posts")
 
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
